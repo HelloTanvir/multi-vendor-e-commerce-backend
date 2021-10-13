868cff6c753529e967383f0d1e1143931ef011b1
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2021-10-13 10:59:41 +0600

    ts-node not reading declaration file fixed

--- src/@types/type.d.ts
+++ src/@types/type.d.ts
@@ -4,11 +4,8 @@ declare global {
     namespace NodeJS {
         interface ProcessEnv {
             PORT: string;
-
             FRONTEND_URL: string;
-
             MONGO_URL: string;
-
             JWT_SECRET: string;
             JWT_EXPIRE: string;
             JWT_COOKIE_EXPIRE: string;
@@ -22,7 +19,3 @@ declare global {
         }
     }
 }
-
-// eslint-disable-next-line prettier/prettier
-export { };
-// eslint-disable-next-line prettier/prettier
