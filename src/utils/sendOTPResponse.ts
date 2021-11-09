4f5dad2724b24b46e8d3c9b63e4ff3822b825f9e
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2021-11-09 16:58:17 +0600

    sms sending enabled

--- src/utils/sendOTPResponse.ts
+++ src/utils/sendOTPResponse.ts
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { Response } from 'express';
 import otpGenerator from 'otp-generator';
 import { RedisClient } from 'redis';
@@ -14,23 +15,23 @@ const sendOTPResponse = async (number: string, statusCode: number, res: Response
 
         (redisClient as RedisClient).setex(number, +process.env.OTP_EXPIRE, `${generatedOtp}`);
 
-        // const otpProviderUrl = `${process.env.OTP_URL}?username=${
-        //     process.env.OTP_USERNAME
-        // }&password=${
-        //     process.env.OTP_PASSWORD
-        // }&number=${number}&message=${process.env.OTP_USERNAME.toUpperCase()} OTP Code is ${generatedOtp}`;
+        const otpProviderUrl = `${process.env.OTP_URL}?username=${
+            process.env.OTP_USERNAME
+        }&password=${
+            process.env.OTP_PASSWORD
+        }&number=${number}&message=${process.env.OTP_USERNAME.toUpperCase()} OTP Code is ${generatedOtp}`;
 
-        // const otpResponse = await axios.post(
-        //     otpProviderUrl,
-        //     {},
-        //     {
-        //         headers: {
-        //             'Content-Type': 'application/x-www-form-urlencoded',
-        //         },
-        //     }
-        // );
+        const otpResponse = await axios.post(
+            otpProviderUrl,
+            {},
+            {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+            }
+        );
 
-        const otpResponse = true;
+        // const otpResponse = true;
 
         if (otpResponse) {
             res.status(statusCode).json({
