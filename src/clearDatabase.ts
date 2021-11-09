cd0c999959b106bde9eedfb3b3bb0f9883321ad8
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2021-11-09 13:19:36 +0600

    clear database

---
+++ src/clearDatabase.ts
@@ -0,0 +1,30 @@
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+// models
+import RefreshToken from './models/refreshToken.model';
+import User from './models/user.model';
+
+// load environment variables
+dotenv.config();
+
+// connect to db
+mongoose.connect(process.env.MONGO_URL);
+
+// delete data
+const deleteData = async () => {
+    try {
+        await User.deleteMany();
+        await RefreshToken.deleteMany();
+        console.log('data destroyed...');
+        process.exit();
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+// command:
+//      node seeder -d (for deleting from database)
+
+if (process.argv[2] === '-d') {
+    deleteData();
+}
