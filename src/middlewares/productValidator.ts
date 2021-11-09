1422b025b3d7fc5a31ad6873ac0cb82f48e9818c
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2021-11-09 23:17:56 +0600

    product validation

---
+++ src/middlewares/productValidator.ts
@@ -0,0 +1,75 @@
+import { check } from 'express-validator';
+
+export const productValidator = [
+    check('vendorId').isLength({ min: 1 }).withMessage('Please input vendor id').trim(),
+
+    check('image').isLength({ min: 1 }).withMessage('Please input product image').trim(),
+
+    check('name').isLength({ min: 1 }).withMessage('Please input product name').trim(),
+
+    check('weight').isLength({ min: 1 }).withMessage('Please input product weight').trim(),
+
+    check('regularPrice')
+        .isLength({ min: 1 })
+        .withMessage('Please input product regular price')
+        .trim(),
+
+    check('salesPrice').isLength({ min: 1 }).withMessage('Please input product sales price').trim(),
+
+    check('inventory').isLength({ min: 1 }).withMessage('Please input product inventory').trim(),
+
+    check('description')
+        .isLength({ min: 1 })
+        .withMessage('Please input product description')
+        .trim(),
+];
+
+export const productUpdateValidator = [
+    check('vendorId')
+        .isLength({ min: 1 })
+        .optional({ nullable: true })
+        .withMessage('Please input vendor id')
+        .trim(),
+
+    check('image')
+        .isLength({ min: 1 })
+        .optional({ nullable: true })
+        .withMessage('Please input product image')
+        .trim(),
+
+    check('name')
+        .isLength({ min: 1 })
+        .optional({ nullable: true })
+        .withMessage('Please input product name')
+        .trim(),
+
+    check('weight')
+        .isLength({ min: 1 })
+        .optional({ nullable: true })
+        .withMessage('Please input product weight')
+        .trim(),
+
+    check('regularPrice')
+        .isLength({ min: 1 })
+        .optional({ nullable: true })
+        .withMessage('Please input product regular price')
+        .trim(),
+
+    check('salesPrice')
+        .isLength({ min: 1 })
+        .optional({ nullable: true })
+        .withMessage('Please input product sales price')
+        .trim(),
+
+    check('inventory')
+        .isLength({ min: 1 })
+        .optional({ nullable: true })
+        .withMessage('Please input product inventory')
+        .trim(),
+
+    check('description')
+        .isLength({ min: 1 })
+        .optional({ nullable: true })
+        .withMessage('Please input product description')
+        .trim(),
+];
--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,5 +1,19 @@
-import { Router } from 'express';
+import { Request, Response, Router } from 'express';
+import { productValidator } from '../middlewares/productValidator';
+import validationHandler from '../middlewares/validationHandler';
 
 const productRouter = Router();
+
+// URL: /v1/products/create-one
+productRouter.post(
+    '/create-one',
+    productValidator,
+    validationHandler,
+    (req: Request, res: Response) => {
+        res.status(200).json({
+            message: 'route for creating a product',
+        });
+    }
+);
 
 export default productRouter;
