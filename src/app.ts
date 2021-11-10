50527bd46157f9e67119b46962679ee14821a751
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2021-11-10 22:43:25 +0600

    cors disabled for development

--- src/app.ts
+++ src/app.ts
@@ -38,9 +38,15 @@ app.use(xss());
 app.use(hpp());
 
 // prevent cors issue
+// app.use(
+//     cors({
+//         origin: process.env.FRONTEND_URL,
+//         credentials: true,
+//     })
+// );
 app.use(
     cors({
-        origin: process.env.FRONTEND_URL,
+        origin: '*',
         credentials: true,
     })
 );
