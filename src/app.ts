8cb2266288cc087708db63c37041bc9233087cf6
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2021-11-17 19:57:24 +0600

    allowed cors for different ports on development

--- src/app.ts
+++ src/app.ts
@@ -38,9 +38,17 @@ app.use(xss());
 app.use(hpp());
 
 // prevent cors issue
+// app.use(
+//     cors({
+//         origin: process.env.FRONTEND_URL,
+//         credentials: true,
+//     })
+// );
+
+// development
 app.use(
     cors({
-        origin: process.env.FRONTEND_URL,
+        origin: ['http://localhost:3000', 'http://localhost:8080'],
         credentials: true,
     })
 );
