8682d1c1897775af1a51e22bf702c1f1c2f1b44e
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2021-12-05 22:05:05 +0600

    unnecessary comment removed

--- src/utils/s3.ts
+++ src/utils/s3.ts
@@ -4,10 +4,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const s3 = new aws.S3({
-    // credentials: {
-    //     accessKeyId: process.env.AWS_ACCESS_KEY,
-    //     secretAccessKey: process.env.AWS_SECRET_KEY,
-    // },
     accessKeyId: process.env.AWS_ACCESS_KEY,
     secretAccessKey: process.env.AWS_SECRET_KEY,
     region: process.env.AWS_REGION,
