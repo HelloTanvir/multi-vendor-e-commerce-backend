0616967ac82accbbb0634921f21decb1661b5361
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2021-12-12 22:31:25 +0600

    cors allowed for new domain

--- src/app.ts
+++ src/app.ts
@@ -52,6 +52,7 @@ app.use(
             'http://localhost:3000',
             'http://localhost:8080',
             'https://quirky-meninsky-8c97db.netlify.app',
+            'https://sellbeez69.netlify.app/',
         ],
         credentials: true,
     })
