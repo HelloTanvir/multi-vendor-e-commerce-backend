5229f1b4e8166e8955d07beb6ea5f3c2f5005c2f
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2021-12-14 16:58:05 +0600

    coupon model created

---
+++ src/models/coupon.model.ts
@@ -0,0 +1,49 @@
+import mongoose, { Document } from 'mongoose';
+
+export interface ICoupon extends Document {
+    vendorId: string;
+    name: string;
+    amount: number;
+    expiryDate: Date;
+    limitPerCoupon: number;
+    limitPerUser: number;
+}
+
+const CouponSchema = new mongoose.Schema<ICoupon>(
+    {
+        vendorId: {
+            type: String,
+            required: [true, 'vendor id is required'],
+        },
+
+        name: {
+            type: String,
+            required: [true, 'Coupon name is required'],
+        },
+
+        amount: {
+            type: Number,
+            required: [true, 'Coupon amount is required'],
+        },
+
+        expiryDate: {
+            type: Date,
+            required: [true, 'Coupon expiry date is required'],
+        },
+
+        limitPerCoupon: {
+            type: Number,
+            required: [true, 'Usage limit per coupon is required'],
+        },
+
+        limitPerUser: {
+            type: Number,
+            required: [true, 'Usage limit per user is required'],
+        },
+    },
+    { timestamps: true }
+);
+
+const Coupon = mongoose.model('Coupon', CouponSchema);
+
+export default Coupon;
