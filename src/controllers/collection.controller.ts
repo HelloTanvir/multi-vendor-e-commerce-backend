abef8f8c7c45d592e44e0ca19258ec666f4c2dac
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2021-12-16 16:27:55 +0600

    controller created for creating coupon

---
+++ src/controllers/collection.controller.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from 'express';
+import Collection from '../models/collection.model';
+
+export const createCollection = async (req: Request, res: Response) => {
+    try {
+        const collection = new Collection({
+            ...req.body,
+        });
+
+        await collection.save();
+
+        res.status(201).json({
+            data: collection,
+        });
+    } catch (error: any) {
+        res.status(error.statusCode || 500).json({
+            errors: {
+                common: {
+                    msg: error.message || 'Server error occured',
+                },
+            },
+        });
+    }
+};
--- src/routes/collection.route.ts
+++ src/routes/collection.route.ts
@@ -1,10 +1,17 @@
 import { Router } from 'express';
+import { createCollection } from '../controllers/collection.controller';
+import collectionValidator from '../middlewares/collectionValidator';
+import validationHandler from '../middlewares/validationHandler';
+import { verifyAccessToken } from '../middlewares/verifyToken';
 
 const collectionRouter = Router();
 
 // URL: /v1/collections
 // URL: /v1/collections?page=1&size=10 (for pagination)
-collectionRouter.route('/').post().get();
+collectionRouter
+    .route('/')
+    .post(collectionValidator, validationHandler, verifyAccessToken, createCollection)
+    .get();
 
 // URL: /v1/collections/1
 collectionRouter.route('/:collectionId').get().patch().delete();
