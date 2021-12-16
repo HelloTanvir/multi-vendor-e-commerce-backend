007b6e459e3a48fd2db625f43c1ed1f079867062
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2021-12-16 21:56:13 +0600

    collection update validator created

--- src/middlewares/collectionValidator.ts
+++ src/middlewares/collectionValidator.ts
@@ -1,9 +1,17 @@
 import { check } from 'express-validator';
 
-const collectionValidator = [
+export const collectionValidator = [
     check('name').isLength({ min: 1 }).withMessage('Please input collection name').trim(),
 
     check('productIds').isArray({ min: 1 }).withMessage('Please input one or more products'),
 ];
 
-export default collectionValidator;
+export const collectionUpdateValidator = [
+    check('name')
+        .isLength({ min: 1 })
+        .optional({ nullable: true })
+        .withMessage('Please input collection name')
+        .trim(),
+
+    check('productIds').isArray({ min: 1 }).withMessage('Please input one or more products'),
+];
