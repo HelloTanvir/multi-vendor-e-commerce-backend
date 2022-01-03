b77a32457f3148f6a098670187793e3de0be5568
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2022-01-03 22:46:14 +0600

    login controller created for customer

--- src/controllers/customer.controller.ts
+++ src/controllers/customer.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
-import Customer from '../models/customer.model';
+import createHttpError from 'http-errors';
+import Customer, { CustomerData } from '../models/customer.model';
 import sendTokenResponse from '../utils/sendTokenResponse';
 
 export const signup = async (req: Request, res: Response) => {
@@ -9,6 +10,32 @@ export const signup = async (req: Request, res: Response) => {
         await customer.save();
 
         await sendTokenResponse(customer, 201, res);
+    } catch (error: any) {
+        res.status(error.statusCode || 500).json({
+            errors: {
+                common: {
+                    msg: error.message || 'Server error occured',
+                },
+            },
+        });
+    }
+};
+
+export const login = async (req: Request, res: Response) => {
+    try {
+        const { email, password } = req.body as CustomerData;
+
+        const customer = await Customer.findOne({ email });
+
+        if (!customer) return;
+
+        const isPasswordMatch = customer.matchPassword(password);
+
+        if (!isPasswordMatch) {
+            throw new createHttpError.BadRequest('Incorrect Password');
+        }
+
+        await sendTokenResponse(customer, 200, res);
     } catch (error: any) {
         res.status(error.statusCode || 500).json({
             errors: {
