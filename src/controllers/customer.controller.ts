5e5ba8e73164c42493326887b174c53e809d93ec
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2022-01-03 22:47:43 +0600

    logout controller created for customer

--- src/controllers/customer.controller.ts
+++ src/controllers/customer.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import createHttpError from 'http-errors';
 import Customer, { CustomerData } from '../models/customer.model';
+import RefreshToken from '../models/refreshToken.model';
 import sendTokenResponse from '../utils/sendTokenResponse';
 
 export const signup = async (req: Request, res: Response) => {
@@ -36,6 +37,33 @@ export const login = async (req: Request, res: Response) => {
         }
 
         await sendTokenResponse(customer, 200, res);
+    } catch (error: any) {
+        res.status(error.statusCode || 500).json({
+            errors: {
+                common: {
+                    msg: error.message || 'Server error occured',
+                },
+            },
+        });
+    }
+};
+
+export const logout = async (req: Request, res: Response) => {
+    try {
+        const refreshToken = await RefreshToken.findOne({ userId: req.userId });
+
+        let deletedToken;
+
+        if (refreshToken) {
+            deletedToken = await refreshToken.delete();
+        }
+
+        if (!deletedToken) throw new createHttpError.InternalServerError('Logout failed');
+
+        res.status(204).clearCookie('access-token').clearCookie('refresh-token').json({
+            success: true,
+            message: 'You are logged out',
+        });
     } catch (error: any) {
         res.status(error.statusCode || 500).json({
             errors: {
