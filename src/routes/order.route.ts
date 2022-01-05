1f2449392b380d23308b431407aba61bd3c3c922
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2022-01-05 21:31:41 +0600

    controllers added on order route

--- src/routes/order.route.ts
+++ src/routes/order.route.ts
@@ -1,4 +1,11 @@
 import { Router } from 'express';
+import {
+    createOrder,
+    getOrders,
+    getSingleOrder,
+    // eslint-disable-next-line prettier/prettier
+    updateOrder
+} from '../controllers/order.controller';
 import { orderValidator } from '../middlewares/order.validator';
 import validationHandler from '../middlewares/validationHandler';
 import { verifyAccessToken } from '../middlewares/verifyToken';
@@ -8,10 +15,10 @@ const orderRouter = Router();
 // URL: /v1/order
 orderRouter
     .route('/')
-    .post(orderValidator, validationHandler, verifyAccessToken('customer'))
-    .get(verifyAccessToken('vendor'));
+    .post(orderValidator, validationHandler, verifyAccessToken('customer'), createOrder)
+    .get(verifyAccessToken('vendor'), getOrders);
 
 // URL: /v1/order/1
-orderRouter.route('/:orderId').get().delete(verifyAccessToken('customer'));
+orderRouter.route('/:orderId').get(getSingleOrder).patch(verifyAccessToken('vendor'), updateOrder);
 
 export default orderRouter;
