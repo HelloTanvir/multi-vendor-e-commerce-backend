d2a5c362364f90aef4b02e1d2f8323b998ac8bd4
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2022-01-05 21:11:48 +0600

    controller created for updating order

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
-import Order from '../models/order.model';
+import createHttpError from 'http-errors';
+import Order, { IOrder } from '../models/order.model';
 
 export const createOrder = async (req: Request, res: Response) => {
     try {
@@ -60,6 +61,35 @@ export const getOrders = async (req: Request, res: Response) => {
             page,
             size,
             data: orders,
+        });
+    } catch (error: any) {
+        res.status(error.statusCode || 500).json({
+            errors: {
+                common: {
+                    msg: error.message || 'Server error occured',
+                },
+            },
+        });
+    }
+};
+
+export const updateOrder = async (req: Request, res: Response) => {
+    try {
+        const { orderId } = req.params as { orderId: string };
+        const { status } = req.body as IOrder;
+
+        const order = await Order.findById(orderId);
+
+        if (!order) {
+            throw new createHttpError.BadRequest('Invalid order id');
+        }
+
+        if (status) order.status = status;
+
+        await order.save();
+
+        res.status(200).json({
+            data: order,
         });
     } catch (error: any) {
         res.status(error.statusCode || 500).json({
